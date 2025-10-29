2f3f3d52714f336a557b5fc393260860dbaff84a
benjamin10ks <bjkmanning@gmail.com>
2025-10-29 15:54:04 -0600

    started table

---
+++ app/components/githublink.tsx
@@ -0,0 +1,14 @@
+import Storetable from "./storetable";
+
+export default function Githublink() {
+  return (
+    <div>
+      <a href="https://github.com/benjamin10ks/nexthw331">Repo link</a>
+      <p>
+        This project showcases a counter component with color changing
+        funcitonality as well as a table with data from fakestoreapi.com.
+      </p>
+      <Storetable />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Counter from "./components/counter";
+import Githublink from "./components/githublink";
 
 export default function Home() {
   return (
     <div>
+      <Githublink />
       <Counter countBy={1} />
       <Counter countBy={2} />
     </div>
